03ea0bedeb1fc330fe50daace0701cfc79bfb033
Agus Sentosa Hermawan <aguss787@proton.me>
2025-01-14 00:18:05 +0800

    feat(nvim): enable all feature for rust-analyzer

--- nvim/lua/agus/plugins/lsp/rustacean.lua
+++ nvim/lua/agus/plugins/lsp/rustacean.lua
@@ -1,48 +1,44 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^5", -- Recommended
-	lazy = false, -- This plugin is already lazy
-	config = function()
-		vim.g.rustaceanvim = {
-			-- Plugin configuration
-			tools = {
-				enable_nextest = true,
-			},
-			-- LSP configuration
-			server = {
-				---@diagnostic disable-next-line: unused-local
-				on_attach = function(client, bufnr)
-					vim.keymap.set("n", "<leader>il", function()
-						vim.cmd.RustLsp("codeAction")
-					end, { buffer = bufnr, desc = "Code action" })
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+        vim.g.rustaceanvim = {
+            -- Plugin configuration
+            tools = {enable_nextest = true},
+            -- LSP configuration
+            server = {
+                ---@diagnostic disable-next-line: unused-local
+                on_attach = function(client, bufnr)
+                    vim.keymap.set("n", "<leader>il", function()
+                        vim.cmd.RustLsp("codeAction")
+                    end, {buffer = bufnr, desc = "Code action"})
 
-					vim.keymap.set("v", "<leader>il", function()
-						vim.cmd.RustLsp("codeAction")
-					end, { buffer = bufnr, desc = "Code action" })
+                    vim.keymap.set("v", "<leader>il", function()
+                        vim.cmd.RustLsp("codeAction")
+                    end, {buffer = bufnr, desc = "Code action"})
 
-					vim.keymap.set("n", "<leader>if", function()
-						vim.cmd.RustFmt()
-					end, { buffer = bufnr, desc = "Format code" })
+                    vim.keymap.set("n", "<leader>if",
+                                   function()
+                        vim.cmd.RustFmt()
+                    end, {buffer = bufnr, desc = "Format code"})
 
-					vim.keymap.set("n", "<leader>or", function()
-						vim.cmd.RustLsp("debuggables")
-					end, { buffer = bufnr, desc = "Debug" })
-				end,
-				default_settings = {
-					-- Rust-analyzer language server configuration
-					["rust-analyzer"] = {
-						-- Disable clippy
-						checkOnSave = {
-							command = "check",
-						},
-						inlayHints = {
-							maxLength = 100,
-						},
-					},
-				},
-			},
-			-- DAP configuration
-			dap = {},
-		}
-	end,
+                    vim.keymap.set("n", "<leader>or", function()
+                        vim.cmd.RustLsp("debuggables")
+                    end, {buffer = bufnr, desc = "Debug"})
+                end,
+                default_settings = {
+                    -- Rust-analyzer language server configuration
+                    ["rust-analyzer"] = {
+                        cargo = {feature = "all"},
+                        -- Disable clippy
+                        checkOnSave = {command = "check"},
+                        inlayHints = {maxLength = 100}
+                    }
+                }
+            },
+            -- DAP configuration
+            dap = {}
+        }
+    end
 }
