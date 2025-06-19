e90c1991bfca07ad4f0005d9fdcbf4b642c1471e
Agus Sentosa Hermawan <aguss787@proton.me>
2025-06-20 01:18:56 +0800

    feat: mcphub auto approve

--- nvim/lua/agus/plugins/codecompanion.lua
+++ nvim/lua/agus/plugins/codecompanion.lua
@@ -20,7 +20,9 @@ return {
             "ravitemer/mcphub.nvim",
             dependencies = {"nvim-lua/plenary.nvim"},
             build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
-            config = function() require("mcphub").setup() end
+            config = function()
+                require("mcphub").setup({auto_approve = true})
+            end
         }, {
             "github/copilot.vim",
             enabled = true,
