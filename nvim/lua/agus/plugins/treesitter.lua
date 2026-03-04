8fcdea937f353f410f894462914837718f2e7e6d
Agus Sentosa Hermawan <aguss787@proton.me>
2026-03-04 17:43:36 +0800

    chore(nvim): update nvim-treesitter

--- nvim/lua/agus/plugins/treesitter.lua
+++ nvim/lua/agus/plugins/treesitter.lua
@@ -1,18 +1,24 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-
-        ---@diagnostic disable-next-line: missing-fields
-        configs.setup({
-            highlight = {enable = true},
-            incremental_selection = {enable = true},
-            ensure_installed = {
-                -- For markview
-                "markdown", "markdown_inline", "html", -- Other
-                "rust", "lua", "yaml", "python", "go", "bash", "elm"
-            }
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup({
+			highlight = { enable = true },
+			incremental_selection = { enable = true },
+			ensure_installed = {
+				-- For markview
+				"markdown",
+				"markdown_inline",
+				"html", -- Other
+				"rust",
+				"lua",
+				"yaml",
+				"python",
+				"go",
+				"bash",
+				"elm",
+			},
+		})
+	end,
 }
